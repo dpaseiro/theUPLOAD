e2a86678aa215881494732c4318f23ca94bfacf2
Daniel <paseiro101@yahoo.com>
2018-07-18 16:43:59 -0400

    change Heroku vars

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const Video         = require('./models/video');
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/module2', {useMongoClient: true})
+  .connect(process.env.MONGODB_URI, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
